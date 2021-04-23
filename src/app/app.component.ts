9c1701e1f57e88d71b4b33df5ff9da5774be8aa6
Nagaraj-Edke <enagaraj9109@gmail.com>
2021-04-23 14:28:06 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForm';
+  registrationForm :FormGroup;
+
+  ngOnInit(){
+    this.registrationForm = new FormGroup({
+      firstname:new FormControl(''),
+      lastname:new FormControl(''),
+
+      areaCode:new FormControl(''),
+      phoneNo:new FormControl(""),
+      email:new FormControl(''),
+      address:new FormGroup({
+        streetAdd1:new FormControl('llll'),
+        streetAdd2:new FormControl(''),
+        city:new FormControl(""),
+        state:new FormControl(''),
+        zipcode:new FormControl('')
+      }),
+      occupation:new FormControl(''),
+      companyName:new FormControl(""),
+      jobTitle:new FormControl(''),
+      workshopDate:new FormControl(''),
+      payment:new FormControl(""),
+      paymentMehod:new FormControl(""),
+      comments:new FormControl(""),
+      reference:new FormControl('')
+
+    })
+  }
 }
